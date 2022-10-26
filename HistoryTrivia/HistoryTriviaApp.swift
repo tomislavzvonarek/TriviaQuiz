eb54c15c81b25ece56c7bf32c6a1f1c237d0dca3
tomislavzvonarek <tomislav.zvonarek@gmail.com>
2022-10-26 21:05:16 +0200

    Initial Commit

---
+++ HistoryTrivia/HistoryTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryTriviaApp.swift
+//  HistoryTrivia
+//
+//  Created by Tomislav Zvonarek on 26.10.2022..
+//
+
+import SwiftUI
+
+@main
+struct HistoryTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
