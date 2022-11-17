f1a2f1b8a4b881e700e3dc335a207f2fa83ef2a8
tomislavzvonarek <tomislav.zvonarek@gmail.com>
2022-11-17 10:48:03 +0100

    Initial commit

--- HistoryTrivia/ContentView.swift
+++ HistoryTrivia/ContentView.swift
@@ -8,19 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let question = Question(questionText: "What was the first computer bug?", possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"], correctAnswerIndex: 2)
+    
+    
+    @State var mainColor = Color(red:20/255, green:28/255, blue: 58/255)
+    let accentColor = Color(red:48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text(question.questionText)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack{
+                    ForEach(0..<question.possibleAnswers.count)
+                    {
+                        answerIndex in
+                        Button(action: {
+                            print("Tapped on option with the text \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        })
+                }
+                    
+                }
+            }
         }
-        .padding()
+        
+        
+        .foregroundColor(.white)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  HistoryTrivia
+//
+//  Created by Tomislav Zvonarek on 17.11.2022..
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
